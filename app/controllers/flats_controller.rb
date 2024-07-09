41a0c88ee53d0fba68bab21571010374628144f5
Dimitra Perissiou <dimitraper85@gmail.com>
2024-07-09 17:12:07 +0200

    adds flats#new

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,4 +3,10 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+  def new
+  end
+
+  # def owner_index
+  #if the current user is an owner
+  #get all the flats of this owner and display them in a list
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :flats, only: [:index, :show]
+  resources :flats, only: [:index, :show, :new]
 
-  get ":user_id/bookings", to: "bookings#index"
+
+  get ":user_id/bookings", to: "bookings#index", as: "user_bookings"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
