cf6dad384b9307d1ec7a5217607779a8c0e9a856
Max Mengeringhausen <max.mengeringhausen.jean@gmail.com>
2024-07-09 17:50:58 +0200

    booking page for user & owner

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,5 +2,6 @@ class BookingsController < ApplicationController
   def index
     @user = User.find(params["user_id"])
     @bookings = Booking.where(user_id: params["user_id"])
+    @my_flats = Flat.where(user_id: params["user_id"])
   end
 end
---
+++ db/migrate/20240709152024_add_status_booking.rb
@@ -0,0 +1,5 @@
+class AddStatusBooking < ActiveRecord::Migration[7.1]
+  def change
+    add_column :bookings, :status, :string, default: "pending", null: false
+  end
+end
