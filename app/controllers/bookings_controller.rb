1a4b93dd6b6ec64021495cf2bb500d0c07790470
Max Mengeringhausen <max.mengeringhausen.jean@gmail.com>
2024-07-09 15:43:57 +0200

    bookings controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,6 @@
+class BookingsController < ApplicationController
+  def index
+    @user = User.find(params["user_id"])
+    @bookings = Booking.where(user_id: params["user_id"])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :flats, only: [:index, :show]
 
+  get ":user_id/bookings", to: "bookings#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
