085d2c027939a337678ef58bb63d070d9279ce95
Dimitra Perissiou <dimitraper85@gmail.com>
2024-07-09 12:03:48 +0200

    puts navbar and footer in every page

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,5 +2,5 @@ class Flat < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :name, :address, :description, :price, presence:true
+  validates :name, :address, :description, :price, presence: true
 end
