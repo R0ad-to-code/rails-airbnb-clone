e0fff0f15d0b59d017f080883d20b69185e28d2f
Judith Prasser <judith.prasser@gmx.de>
2024-07-11 17:57:13 +0200

    search implemented

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,8 +2,16 @@ class Flat < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
+  include PgSearch::Model
+  pg_search_scope :search_by_address,
+    against: [ :address ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+      }
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
   validates :name, :address, :description, :price, presence: true
+
 end
