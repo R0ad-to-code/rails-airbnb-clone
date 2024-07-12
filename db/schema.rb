5e59f53afe452802b5205164a8d20590b0ec8225
Dimitra Perissiou <dimitraper85@gmail.com>
2024-07-12 12:16:07 +0200

    adds missing end in application controller

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_12_084726) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "owner?"
+    t.boolean "owner?", default: false
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
