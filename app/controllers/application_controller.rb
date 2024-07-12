f351a5d8ab1294f176c2527631915f7c05c6a2d7
Max Mengeringhausen <max.mengeringhausen.jean@gmail.com>
2024-07-12 11:27:08 +0200

    user add flat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, key: :name)
   end
 end
